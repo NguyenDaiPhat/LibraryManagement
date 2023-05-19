6977d66baeb2d0db12e76f6a9be6626ab35aaf23
Lineinuniv <daiphat2442003@gmail.com>
2023-05-19 15:30:08 +0700

    push

---
+++ src/main/java/com/phenikaa/librarymanagement/LibraryManagement.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.phenikaa.librarymanagement;
+
+import View.Login;
+
+/**
+ *
+ * @author Nguyen Dai Phat
+ */
+public class LibraryManagement {
+
+    public static void main(String[] args) {
+        Login login = new Login();
+        login.setVisible(true);
+        login.setLocationRelativeTo(null);
+    }
+}
